b39c1e4adbeb158379c3d814a581bbb36501a90d
augusto.indra <ahenriqueo@minsait.com>
2023-09-26 15:04:48 -0300

    dando continuidade ao crud

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+},
+{
+  path: "products",
+  component: ProductCrudComponent
+},
+{
+  path: "products/create",
+  component: ProductCreatComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
